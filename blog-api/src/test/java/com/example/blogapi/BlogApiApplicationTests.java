08180b56a54d6d27cceeeb82660f809991f9f512
defeng.chen <defeng.chen@sophgo.com>
2023-03-27 15:15:26 +0800

    fastdfs-demo

--- blog-api/src/test/java/com/example/blogapi/BlogApiApplicationTests.java
+++ blog-api/src/test/java/com/example/blogapi/BlogApiApplicationTests.java
@@ -4,17 +4,31 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.blogapi.dao.mapper.ArticleMapper;
 import com.example.blogapi.service.ArticleService;
 import com.example.blogapi.service.impl.ArticleServiceImpl;
+import com.example.blogapi.utils.FastdfsUtils;
 import com.example.blogapi.vo.Result;
 import com.example.blogapi.vo.params.PageParams;
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
 
 @SpringBootTest
+@Slf4j
 class BlogApiApplicationTests {
 
     @Autowired
     private ArticleService articleService;
+
+    @Autowired
+    private FastdfsUtils fastdfsUtils;
+
     @Test
     void contextLoads() {
     }
@@ -37,6 +51,26 @@ class BlogApiApplicationTests {
         params.setPageSize(3);
         articleService.listArticle(params);
 
+    }
+
+
+    @Test
+    public   void TestFastDfsUpload() throws IOException {
+        File file = new File("uploadfile/50da7e17-c73f-41f2-a21f-83e634ab201dQQ截图20220815215222.png");
+        InputStream inputStream = new FileInputStream(file);
+        MultipartFile multipartFile = new MockMultipartFile(file.getName(),file.getName(),"text",inputStream);
+        String storePath = fastdfsUtils.upload(multipartFile);
+        log.info("storePath:[{}]",storePath);
+        //StorePath [group=group1, path=M00/00/00/CgAQAmQhQDCAUqyfAACde9yfmf4145.png]
+    }
+
+    @Test
+    public  void TestDownload() throws IOException {
+        String path = "group1/M00/00/00/CgAQAmQhQgaAIJ-EAACde9yfmf4793.png";
+        byte[] download = fastdfsUtils.download(path, "");
+        FileOutputStream fileOutputStream = new FileOutputStream("uploadfile/kk.png");
+        fileOutputStream.write(download);
+        fileOutputStream.close();
     }
 
 }
