beb44b8c59f1610c53855564c0e2f8d5e58500e8
kfc966 <1797004658@qq.com>
2023-04-16 17:31:17 +0800

    dev-eruka

--- blog-api/src/main/java/com/example/blogapi/BlogApiApplication.java
+++ blog-api/src/main/java/com/example/blogapi/BlogApiApplication.java
@@ -2,11 +2,13 @@ package com.example.blogapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.retry.annotation.EnableRetry;
 
 
 @SpringBootApplication
 @EnableRetry
+@EnableEurekaClient
 public class BlogApiApplication {
 
     public static void main(String[] args) {
